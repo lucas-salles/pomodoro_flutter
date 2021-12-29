47ad6796cf87b9ca9f5f244f3654b4cc1ee74a2f
Lucas Sales <lucassales398@gmail.com>
2021-12-29 13:31:42 -0300

    Componente EntradaTempo #01

--- lib/components/entrada_tempo.dart
+++ lib/components/entrada_tempo.dart
@@ -10,9 +10,50 @@ class EntradaTempo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
       children: [
-        Text(titulo),
-        Text(valor.toString()),
+        Text(
+          titulo,
+          style: const TextStyle(
+            fontSize: 25,
+          ),
+        ),
+        const SizedBox(height: 10),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {},
+              child: const Icon(
+                Icons.arrow_downward,
+                color: Colors.white,
+              ),
+              style: ElevatedButton.styleFrom(
+                shape: const CircleBorder(),
+                padding: const EdgeInsets.all(15),
+                primary: Colors.red,
+              ),
+            ),
+            Text(
+              '$valor min',
+              style: const TextStyle(
+                fontSize: 18,
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Icon(
+                Icons.arrow_upward,
+                color: Colors.white,
+              ),
+              style: ElevatedButton.styleFrom(
+                shape: const CircleBorder(),
+                padding: const EdgeInsets.all(15),
+                primary: Colors.red,
+              ),
+            ),
+          ],
+        ),
       ],
     );
   }
