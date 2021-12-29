e08d18fc36914f2a7dd383ced8e0d014aab3e3bf
Lucas Sales <lucassales398@gmail.com>
2021-12-29 15:01:18 -0300

    Pomodoro Store #04

--- lib/store/pomodoro.store.dart
+++ lib/store/pomodoro.store.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:mobx/mobx.dart';
 
 part 'pomodoro.store.g.dart';
@@ -25,19 +27,32 @@ abstract class _PomodoroStore with Store {
   @observable
   TipoIntervalo tipoIntervalo = TipoIntervalo.DESCANSO;
 
+  Timer? cronometro;
+
   @action
   void iniciar() {
     iniciado = true;
+    cronometro = Timer.periodic(const Duration(milliseconds: 50), (timer) {
+      if (minutos == 0 && segundos == 0) {
+        _trocarTipoIntervalo();
+      } else if (segundos == 0) {
+        segundos = 59;
+        minutos--;
+      } else {
+        segundos--;
+      }
+    });
   }
 
   @action
   void parar() {
     iniciado = false;
+    cronometro?.cancel();
   }
 
   @action
   void reiniciar() {
-    iniciado = false;
+    parar();
   }
 
   @action
@@ -63,4 +78,15 @@ abstract class _PomodoroStore with Store {
   bool estaTrabalhando() => tipoIntervalo == TipoIntervalo.TRABALHO;
 
   bool estaDescansando() => tipoIntervalo == TipoIntervalo.TRABALHO;
+
+  void _trocarTipoIntervalo() {
+    if (estaTrabalhando()) {
+      tipoIntervalo = TipoIntervalo.DESCANSO;
+      minutos = tempoDescanso;
+    } else {
+      tipoIntervalo = TipoIntervalo.TRABALHO;
+      minutos = tempoTrabalho;
+    }
+    segundos = 0;
+  }
 }
